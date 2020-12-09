7e0abd7152ebad8c5174b4473f4d302c362cc823
Tanimara Elias Santos <36615712+anthropovixen@users.noreply.github.com>
2020-12-09 21:55:38 +1100

    Update scripts.js
    Add code to highlight big and average height pokemons.

--- js/scripts.js
+++ js/scripts.js
@@ -9,5 +9,11 @@ let pokemonList = [
 ];
 
 for(let i = 0; i < pokemonList.length; i++) {
-    document.write('<p>' + pokemonList[i].name + ' ' + 'height' + ' ' + pokemonList[i].height) + '</p>';
+    if (pokemonList[i].height > 4) {
+        document.write('<p>' + pokemonList[i].name + ' height ' + pokemonList[i].height + ' That\'s a big pokemon!' + '</p>');
+    } else if (pokemonList[i].height > 2 && pokemonList[i].height <= 4 ) {
+        document.write('<p>' + pokemonList[i].name + ' height ' + pokemonList[i].height + ' That\'s an average pokemon!' + '</p>' );
+    } else {
+    document.write('<p>' + pokemonList[i].name + ' height ' + pokemonList[i].height + '</p>');
+    }
 }
