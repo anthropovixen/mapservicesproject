2397ef6f57fc845ae97d661794899d8ede5b08b6
Tanimara Elias Santos <36615712+anthropovixen@users.noreply.github.com>
2020-12-09 21:22:01 +1100

    Update scripts.js
    For loop to extract names and heights of pokemon's list

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,17 @@
 let pokemonList = [
-    {name:"venomoth", height: 4, types: ["bug", "poison"]
+    {name:'venomoth', height: 4, types: ['bug', 'poison']
     },
-    {name: "charizard", height: 5, types: ["fire", "flying"]
+    {name: 'charizard', height: 5, types: ['fire', 'flying']
     },
-    {name: "baltoy", height: 1, types: ["ground", "psychic"]
+    {name: 'baltoy', height: 1, types: ['ground', 'psychic']
     },
-    {name: "diglett", height: 1, types: "ground"}
+    {name: 'diglett', height: 1, types: 'ground'}
 ];
+
+for(let i = 0; i < pokemonList.length; i++) {
+    document.write('<p>' + pokemonList[i].name + ' ' + 'height' + ' ' + pokemonList[i].height) + '</p>';
+}
+
 document.write(pokemonList);
 pokemonList[0];
 pokemonList[0].height;
