3f6ede8eed8b34bd24191a1cea2b9ccfdeb8b84f
Tanimara Elias Santos <36615712+anthropovixen@users.noreply.github.com>
2020-12-16 15:06:48 +1100

    Update scripts.js
    Create variable PokemonRepository as IIFE from PokemonList.
    Add new pokemon to array.
    Return all items from array.
    Create forEach loop from PokemonRepository variable.

--- js/scripts.js
+++ js/scripts.js
@@ -1,19 +1,57 @@
-let pokemonList = [
-    {name:'venomoth', height: 4, types: ['bug', 'poison']
-    },
-    {name: 'charizard', height: 5, types: ['fire', 'flying']
-    },
-    {name: 'baltoy', height: 1, types: ['ground', 'psychic']
-    },
-    {name: 'diglett', height: 1, types: 'ground'}
-];
+// let pokemonList = [
+//     {name:'venomoth', height: 4, types: ['bug', 'poison']
+//     },
+//     {name: 'charizard', height: 5, types: ['fire', 'flying']
+//     },
+//     {name: 'baltoy', height: 1, types: ['ground', 'psychic']
+//     },
+//     {name: 'diglett', height: 1, types: 'ground'}
+// ];
 
-for(let i = 0; i < pokemonList.length; i++) {
-    if (pokemonList[i].height > 4) {
-        document.write('<p>' + pokemonList[i].name + ' height ' + pokemonList[i].height + ' That\'s a big pokemon!' + '</p>');
-    } else if (pokemonList[i].height > 2 && pokemonList[i].height <= 4 ) {
-        document.write('<p>' + pokemonList[i].name + ' height ' + pokemonList[i].height + ' That\'s an average pokemon!' + '</p>' );
-    } else {
-    document.write('<p>' + pokemonList[i].name + ' height ' + pokemonList[i].height + '</p>');
-    }
-}
+// pokemonList.forEach (function (pokemon) {
+//     document.write('<p>' + 'The ' + pokemon.name + ' is ' + pokemon.height + ' inches tall. ' + '</p>')
+// })
+// for(let i = 0; i < pokemonList.length; i++) {
+//     if (pokemonList[i].height > 4) {
+//         document.write('<p>' + pokemonList[i].name + ' height ' + pokemonList[i].height + ' That\'s a big pokemon!' + '</p>');
+//     } else if (pokemonList[i].height > 2 && pokemonList[i].height <= 4 ) {
+//         document.write('<p>' + pokemonList[i].name + ' height ' + pokemonList[i].height + ' That\'s an average pokemon!' + '</p>' );
+//     } else {
+//     document.write('<p>' + pokemonList[i].name + ' height ' + pokemonList[i].height + '</p>');
+//     }
+// }
+
+let PokemonRepository = (function() {
+    let pokemonList = [
+        {name:'venomoth', height: 4, types: ['bug', 'poison']
+        },
+        {name: 'charizard', height: 5, types: ['fire', 'flying']
+        },
+        {name: 'baltoy', height: 1, types: ['ground', 'psychic']
+        },
+        {name: 'diglett', height: 1, types: 'ground'}
+    ];
+
+    function getAll() {
+        return pokemonList;
+    };
+
+    function add(item) {
+        pokemonList.push(item);
+    };
+
+    return {
+        add: add,
+        getAll: getAll
+    };
+    
+}) ();
+
+console.log(PokemonRepository.getAll()); //see what's inside the new repository
+PokemonRepository.add({name: 'blastoise', height: 5, types: ['grass', 'electric']}); //add new pokemon to the repository
+console.log(PokemonRepository.getAll()); // see repository with the alteration
+
+
+PokemonRepository.getAll().forEach (function (pokemon) {
+    document.write('<p>' + 'The ' + pokemon.name + ' pokemon is ' + pokemon.height + ' inches tall. ' + '</p>')
+})
